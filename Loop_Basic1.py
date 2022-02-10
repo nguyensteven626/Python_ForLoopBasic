1878e68a4976a2e0f9cc13f182f5d42bbf636313
nguyensteven626 <90593359+nguyensteven626@users.noreply.github.com>
2022-02-10 15:52:52 -0800

    Loop Basic Commit

---
+++ Loop_Basic1.py
@@ -0,0 +1,37 @@
+for x in range(151):{
+    print(x)
+}
+for x in range(5,1000,5):
+    print(x)
+
+ 
+def codingdojo():
+    for x in range (1,101):
+        if x % 5 == 0:
+            print ('Coding')
+        if x % 10 == 0:
+            print ('Coding Dojo')
+
+codingdojo()
+
+sum = 0
+for x in range(0,500000):
+    if (x%2 !=0):
+        sum = sum + x
+        print(sum)
+
+start = 2018
+end = 0
+def decreasebyfour():{
+    x = 2018
+    while x > 0:
+        print(x)
+        x = x -4
+decreasebyfour(
+
+lowNum = 3
+highNum = 21 
+mult = 3
+for x in range(lowNum,highNum):
+    if x%3 == 0:
+        print(x)
